275874cff7b874a54fa02a09409f7544785b7a22
Natanael Ureña Fernandez <snake0997@hotmail.com>
2021-05-20 09:27:46 -0400

    Uploading the proyect... ❤

---
+++ Notes/Notes/Data/NoteDatabase.cs
@@ -0,0 +1,47 @@
+﻿using Notes.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Data
+{
+    public class NoteDatabase
+    {
+        readonly SQLiteAsyncConnection database;
+
+        public NoteDatabase(string databasePath)
+        {
+            database = new SQLiteAsyncConnection(databasePath);
+            database.CreateTableAsync<Note>().Wait();
+        }
+
+        public Task<List<Note>> GetNotesAsync()
+        {
+            return database.Table<Note>().ToListAsync();
+        }
+
+        public Task<Note> GetNoteAsync(int id)
+        {
+            return database.Table<Note>().Where(n => n.ID == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveNoteAsync(Note note)
+        {
+            if (note.ID != 0)
+            {
+                return database.UpdateAsync(note);
+            } 
+            else
+            {
+                return database.InsertAsync(note);
+            }
+        }
+
+        public Task<int> DeleteNoteAsync(Note note)
+        {
+            return database.DeleteAsync(note);
+        }
+    }
+}
---
+++ Notes/Notes/Views/NoteEntryPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Notes.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes.Views
+{
+    /* 
+     * QueryProperty enables data to be passed into the page, during navigation, via query parameters. 
+     * The first argument for the QueryPropertyAttribute specifies the name of the property that will 
+     * receive the data, with the second argument specifying the query parameter id. 
+     */
+    [QueryProperty(nameof(ItemId), nameof(ItemId))]
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new Note();
+        }
+
+        public string ItemId
+        {
+            set
+            {
+                LoadNote(value);
+            }
+        }
+
+        async void LoadNote(string itemId)
+        {
+            try
+            {
+                int id = Convert.ToInt32(itemId);
+                Note note = await App.Database.GetNoteAsync(id);
+
+                BindingContext = note;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to load note...", e);
+            }
+        }
+
+        async void OnSaveNoteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            note.Date = DateTime.UtcNow;
+            if (!string.IsNullOrWhiteSpace(note.Text))
+            {
+                await App.Database.SaveNoteAsync(note);
+            }
+
+            // Navigate backwards
+            await Shell.Current.GoToAsync("..");
+        }
+
+        async void OnDeleteNoteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            await App.Database.DeleteNoteAsync(note);
+            
+            // Navigate backwards
+            await Shell.Current.GoToAsync("..");
+        }
+    }
+}
